33aba4c989c2e64631879fec9b32c74b4d08aca7
chearie <59700265+chearie@users.noreply.github.com>
2020-01-25 13:30:51 -0500

    Add files via upload

---
+++ devoir1.py
@@ -0,0 +1,32 @@
+# 2020 Ariane Chevrier
+# coding: utf-8
+url = "https://montrealcampus.ca?p=30145"
+# print(url)
+# print(url.find("="))
+# print(url[url.find("=")+1:])
+chiffre = url.find("=")
+url1 = (url[:url.find("=")+1])
+# print(url1)
+code = (url[url.find("=")+1:])
+# print(code)
+code1 = code.replace(code,"20000")
+# print(code1)
+code2 = code.replace(code,"30150")
+# # print(code2)
+# jolieListe = [code1, code2]
+# print(jolieListe)
+# grandeListe = list(range(20000,30151))
+# print(grandeListe)
+n = 0
+# for articles in grandeListe:
+#     n += 1
+#     print(n,url1+str(articles))
+# print(grandeListe)
+urlFinaux = []
+for articles in range(20000,30151) :
+    n += 1
+    print(n, url1+str(articles))
+    urlFinaux.append(url1+str(articles))
+
+print(urlFinaux)
+print(len(urlFinaux))
